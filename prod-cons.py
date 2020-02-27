efc03fd1db485abc9bb2511a8f6419e1a18d463e
sebasped <sebasped@disroot.org>
2020-02-27 10:27:46 -0300

    el ej de monitor

---
+++ prod-cons.py
@@ -0,0 +1,48 @@
+import threading
+import time
+import logging
+
+logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message)s', datefmt='%H:%M:%S', level=logging.INFO)
+
+
+def productor(monitor):
+    print("I'm the producer")
+    for i in range(30):
+        with monitor:          # hace el acquire y al final un release
+            items.append(i)    # agrega un ítem
+            monitor.notify()   # Notifica que ya se puede hacer acquire
+        time.sleep(1)
+
+
+class consumer(threading.Thread):
+    def __init__(self, monitor):
+        super().__init__()
+        self.flag = True
+        self.monitor = monitor
+
+    def run(self):
+        while (True):
+            with self.monitor:          # Hace el acquire y al final un release
+                while len(items)<1:     # si no hay ítems para consumir
+                    self.monitor.wait()  # espera la señal, es decir el notify
+            x = items.pop(0)     # saca (consume) el primer ítem
+            logging.info(f'Consumí {x}')
+            time.sleep(3)
+
+
+# la lista de ítems a consumir
+items = []
+
+# El monitor
+items_monit = threading.Condition()
+
+# un thread que consume
+cons1 = consumer(items_monit)
+cons1.start()
+
+# El productor
+productor(items_monit)
+
+
+
+        
