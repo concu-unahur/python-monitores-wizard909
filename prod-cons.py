7b2428fed77144dc3132e7039be2e9940492a1a7
wizardo909 <cicherofacundo9@gmail.com>
2020-02-27 21:34:19 -0300

    productor y consumidor listo, se puede mejorar para que se ejecuten los consumidores que mas elementos consumen

--- prod-cons.py
+++ prod-cons.py
@@ -7,7 +7,7 @@ logging.basicConfig(format='%(asctime)s.%(msecs)03d [%(threadName)s] - %(message
 
 def productor(monitor):
     print("Voy a producir")
-    for i in range(30):
+    for i in range(10):
         with monitor:          # hace el acquire y al final un release
             items.append(i)    # agrega un ítem
             monitor.notify()   # Notifica que ya se puede hacer acquire
@@ -15,19 +15,26 @@ def productor(monitor):
 
 
 class Consumidor(threading.Thread):
-    def __init__(self, monitor):
+    def __init__(self, monitor,cant):
         super().__init__()
         self.monitor = monitor
+        self.cant = cant
 
     def run(self):
         while (True):
-            
+
+            xx = []
+
             with self.monitor:          # Hace el acquire y al final un release    
-                while len(items)<1:     # si no hay ítems para consumir
+                while len(items)< self.cant:     # si no hay ítems para consumir
                     self.monitor.wait()  # espera la señal, es decir el notify
-                x = items.pop(0)     # saca (consume) el primer ítem
-            
-            logging.info(f'Consumí {x}')
+                
+                for i in range (self.cant):
+                    x = items.pop(0)     # saca (consume) el primer ítem
+                    xx.append(x)
+       
+            logging.info(f'Consumi {xx}')
+
             time.sleep(1)
 
 
@@ -38,11 +45,18 @@ items = []
 items_monit = threading.Condition()
 
 # un thread que consume
-cons1 = Consumidor(items_monit)
+cons1 = Consumidor(items_monit,1)
+cons2 = Consumidor(items_monit,2)
+cons3 = Consumidor(items_monit,3)
+
 cons1.start()
+cons2.start()
+cons3.start()
 
 # El productor
 productor(items_monit)
+
+
 
 
 
